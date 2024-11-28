7e478f588f65aa9435109478bc59a423b6bb574c
404 <sametalpdeger9@gmail.com>
2024-11-28 21:44:59 +0300

    0.0.6

--- src/namespaces/host/funcs/onHostAuth.ts
+++ src/namespaces/host/funcs/onHostAuth.ts
@@ -76,17 +76,19 @@ export const onHostAuth = async <T extends SocketAll>(
     s.emit('host-joined', s.data.isLogged ? s.data.user : s.data.guest)
 
     s.on('disconnect', async () => {
-      console.log('host-left')
-      if (!hasPass) await redisDb.srem('active_public_rooms', roomID)
-      await redisDb.set(`room:${roomID}:game_started`, '0')
-      await redisDb.set(`room:${roomID}:host_in_room`, '0')
-      await redisDb.decr(`room:${roomID}:total_connections`)
-      io.of('/p').to(roomID).emit('host-left')
-      io.of('/p').to(roomID).disconnectSockets()
+      const isInRoom = await redisDb.get(`room:${roomID}:host_in_room`)
+      if (isInRoom === '1') {
+        if (!hasPass) await redisDb.srem('active_public_rooms', roomID)
+        await redisDb.set(`room:${roomID}:host_in_room`, '0')
+        await redisDb.decr(`room:${roomID}:total_connections`)
+        io.of('/p').to(roomID).emit('host-left')
+        io.of('/p').to(roomID).disconnectSockets()
+      }
     })
       ; (s as HostSocket).data.roomID = roomID
     s.join(roomID)
     await redisDb.set(`room:${roomID}:host_in_room`, '1')
+    await redisDb.set(`room:${roomID}:game_started`, '0')
     if (!hasPass) await redisDb.sadd('active_public_rooms', roomID)
     cbs?.beforeRes?.(s as ResultS<T>)
     emitIO().output(hostAuthSchema).emit(s, 'host-auth', {
