3c3f515b002976bb20cccd018759ae074e815a0f
404 <sametalpdeger9@gmail.com>
2024-12-19 13:50:54 +0300

    http

--- src/io.ts
+++ src/io.ts
@@ -2,34 +2,35 @@ import { Server, type ServerOptions } from 'socket.io'
 import { fileURLToPath } from 'node:url'
 import { VERSION } from './constants'
 import { redisDb } from './db/redis'
-import express from 'express'
+// import express from 'express'
 import path from 'node:path'
 import { env } from './env'
 import chalk from 'chalk'
 
 const port = parseInt(env.PORT)
-const app = express()
+// const app = express()
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 const server = await (async () => {
-  if (env.NODE_ENV === 'development') {
-    const createServer = (await import('node:http')).createServer
+  const createServer = (await import('node:http')).createServer
 
-    return createServer(app)
-  } else if (env.NODE_ENV === 'production') {
-    const { createServer } = await import('https')
-    const fs = await import('fs')
+  return createServer()
 
-    return createServer(
-      {
-        cert: fs.readFileSync(`${__dirname}/ssl/domain.cert.pem`),
-        key: fs.readFileSync(`${__dirname}/ssl/private.key.pem`),
-      },
-      app,
-    )
-  }
+  // if (env.NODE_ENV === 'development') {
+  // } else if (env.NODE_ENV === 'production') {
+  //   const { createServer } = await import('https')
+  //   const fs = await import('fs')
+
+  //   return createServer(
+  //     {
+  //       cert: fs.readFileSync(`${__dirname}/ssl/domain.cert.pem`),
+  //       key: fs.readFileSync(`${__dirname}/ssl/private.key.pem`),
+  //     },
+  //     app,
+  //   )
+  // }
 })()
 
 const serverOptions: Partial<ServerOptions> = {
